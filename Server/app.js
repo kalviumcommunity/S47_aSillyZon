6a85010cdd2c4262226b479072ebb883d41c547c
somuyakhandelwal <somuyakhandelwal@gmail.com>
2024-02-15 15:20:17 +0530

    Cors enabled

--- Server/app.js
+++ Server/app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const productRoutes = require("./src/routes/productRoutes");
 const userRoutes = require("./src/routes/userRoutes");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 // Use middleware
 app.use(bodyParser.json());
 // Implement other middleware as needed
